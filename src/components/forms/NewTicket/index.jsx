08c52c84bfc887811ad86c68f5c2aa60adfb149a
Ed <edward.blewitt2@gmail.com>
2024-05-07 17:09:04 +0100

    Implemented engineering tickets

--- src/components/forms/NewTicket/index.jsx
+++ src/components/forms/NewTicket/index.jsx
@@ -1,26 +1,55 @@
-import { useRef } from "react"
+import { useCallback, useRef, useState } from "react"
+import { usePocket } from "../../../contexts/pocketContext"
 
 export function NewTicket({ setTrigger }) {
 
     const ticketRef = useRef()
 
-    function submit(e) {
+    const { pb, user } = usePocket()
+    const [ err, setErr ] = useState(false)
+    const [ success, setSuccess ] = useState(false)
+
+    const submit = useCallback(e => {
         e.preventDefault()
 
-        console.log("New Ticket")
-    }
+        setErr(false)
 
-    return (
+        pb.collection('tickets').create({
+            "user": user.id,
+            "info": ticketRef.current.value
+        })
+        .then(() => {
+            setSuccess(true)
+        })
+        .catch(err => {
+            console.error("Error creating ticket", err)
+            setErr(true)
+        })
+    }, [ pb, user, err, success ])
+
+    return !success ? (
         <form className="form flex col gap-s" onSubmit={submit}>
+
+            <p>Found a bug, or want a feature? Create an engineering ticket.</p>
+
             <div>
                 <div>
                     <label>Engineering Ticket</label>
                 </div>
-                <input type="text" ref={ticketRef} />
+                <input type="text" minLength={10} maxLength={300} ref={ticketRef} required />
             </div>
+            {
+                err ? (
+                    <p>Something went wrong!</p>
+                ) : (
+                    <></>
+                )
+            }
             <div>
                 <button type="submit">Submit</button>
             </div>
         </form>
+    ) : (
+        <p>Ticket received. Thank you!</p>
     )
 }
