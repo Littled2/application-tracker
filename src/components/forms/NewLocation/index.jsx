d50e1f09a0a8e2396227e75bdac7b37393bb617c
Ed <edward.blewitt2@gmail.com>
2024-07-24 18:52:40 +0100

    Can now pinpoint a location on the map of the UK when creating it

--- src/components/forms/NewLocation/index.jsx
+++ src/components/forms/NewLocation/index.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useRef, useState } from "react"
 import { usePocket } from "../../../contexts/pocketContext"
+import ukImage from "./UK.png"
+import styles from "./styles.module.css"
 
 export function NewLocation({ setTrigger, setLocations, sc }) {
 
     const { pb, user } = usePocket()
 
     const [ name, setName ] = useState('')
+
+    const [ distX, setDistX ] = useState(50)
+    const [ distY, setDistY ] = useState(50)
 
     const newInput = useRef()
 
@@ -15,7 +20,9 @@ export function NewLocation({ setTrigger, setLocations, sc }) {
         e.preventDefault()
         pb.collection("locations").create({
             name: name,
-            user: user.id
+            user: user.id,
+            distX,
+            distY
         })
         .then(res => {
             console.log("Created new location", res)
@@ -32,13 +39,42 @@ export function NewLocation({ setTrigger, setLocations, sc }) {
         <form className="form">
 
             <div>
-                <label>Location</label>
+                <label>Location name</label>
             </div>
             <div>
-                <input ref={newInput} value={name} onChange={e => setName(e.target.value)} type="text" />
+                <input ref={newInput} value={name} onChange={e => setName(e.target.value)} type="text" required />
             </div>
 
-            <br />
+            <br /><br />
+
+            <div style={{ position: "sticky", top: "0px", zIndex: "100", textAlign: "center" }}>
+                <label>Please click on the map to mark where this location is</label>
+            </div>
+
+            <div>
+                <div className={styles.imageWrapper} onClick={e => {
+
+                    const rect = e.target.getBoundingClientRect()
+                    const x = e.clientX - rect.left
+                    const y = e.clientY - rect.top
+
+                    setDistY((y / rect.height) * 100)
+                    setDistX((x / rect.width) * 100)
+                }}>
+                    <div className={styles.dotsWrapper}>
+                        
+                            <div className={styles.dot} style={{
+                                    width: `8px`,
+                                    height: `8px`,
+                                    left: `${distX}%`,
+                                    top: `${distY}%`,
+                                    transform: `translateY(calc(-8px / 2)) translateX(calc(-8px / 2))`
+                                }}></div>
+                            </div>
+
+                    <img className={styles.image} src={ukImage} />
+                </div>
+            </div>
 
             <button onClick={submit}>Submit</button>
 
