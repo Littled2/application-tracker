f5c14337cc2b3ab3bc752e62ca59e39a504b4589
Ed <edward.blewitt2@gmail.com>
2024-08-14 02:16:41 +0100

    Can now edit and delete groups

--- src/components/Account/index.jsx
+++ src/components/Account/index.jsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react"
 import { FiDelete } from "react-icons/fi"
 import { BsTrash } from "react-icons/bs"
 import { useActiveYear } from "../../contexts/activeYearContext"
+import { Popup } from "../Popup"
+import { Confirm } from "../forms/Confirm"
 
 export function Account({ setTrigger }) {
 
@@ -12,6 +14,8 @@ export function Account({ setTrigger }) {
     const { pb } = usePocket()
 
     const [ years, setYears ] = useState([])
+    const [ edit, setEdit ] = useState()
+    const [ toDelete, setToDelete ] = useState()
 
     const { clearActiveYears } = useActiveYear()
 
@@ -19,13 +23,23 @@ export function Account({ setTrigger }) {
         pb.collection("years").getFullList()
         .then(yrs => setYears(yrs))
         .catch(err => console.error("Error getting years", err))
+
+        pb.collection('years').subscribe('*', () => {
+
+            pb.collection("years").getFullList()
+            .then(yrs => setYears(yrs))
+            .catch(err => console.error("Error getting years", err))
+
+        }, { /* other options like expand, custom headers, etc. */ });
+
+        return () => pb.collection('years').unsubscribe()
     }, [])
 
 
     return (
         <section className={styles.wrapper}>
-            {/* <div>
-                <p>Years you have registered</p>
+            <div className={styles.section}>
+                <h5>Groups you have registered</h5>
                 <table>
                     <thead>
                         <th>Year</th>
@@ -39,10 +53,10 @@ export function Account({ setTrigger }) {
                                     <tr>
                                         <td>{yr.year}</td>
                                         <td>
-                                            <button><BiEdit /></button>
+                                            <button onClick={() => setEdit(yr)}><BiEdit /></button>
                                         </td>
                                         <td>
-                                            <button><BsTrash /></button>
+                                            <button onClick={() => setToDelete(yr)}><BsTrash /></button>
                                         </td>
                                     </tr>
                                 )
@@ -50,15 +64,55 @@ export function Account({ setTrigger }) {
                         }
                     </tbody>
                 </table>
-            </div> */}
-            <div>
-                <p>Log out of your account</p>
+            </div>
+
+            <div className={styles.section}>
+                <h5>Log out of your account</h5>
                 <button onClick={() => {
                     setTrigger(false)
                     logout()
                     clearActiveYears()
                 }}><BiLogOut /> Logout</button>
             </div>
+
+
+
+            {
+                edit && (
+                    <Popup trigger={edit} setTrigger={setEdit}>
+                        <div className={styles.section}>
+                            <h5>Change name</h5>
+
+                            <div>
+                                <input type="text" value={edit?.year} onInput={e => setEdit(val => {
+                                    return {
+                                        ...val,
+                                        year: e.target.value
+                                    }
+                                })} />
+                            </div>
+
+                            <button
+                                onClick={() => {
+                                    pb.collection("years").update(edit?.id, {
+                                        year: edit?.year
+                                    }).then(() => setEdit(null))
+                                }}
+                            >Save</button>
+                        </div>
+                    </Popup>
+                )
+            }
+
+            {
+                toDelete && (
+                    <Confirm message={"Are you sure you want to delete " + toDelete?.year} trigger={toDelete} setTrigger={setToDelete} onConfirm={() => {
+                            pb.collection("years").delete(toDelete?.id)
+                            .then(() => setToDelete(null))
+                        }}
+                    />
+                )
+            }
         </section>
     )
 }
